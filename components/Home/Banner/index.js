0ac4b8d5181438189f5942fd7ccf55008d91d328
lutfor779 <mdlrrahman779@gmail.com>
2022-08-07 22:20:42 +0600

    tailwind css configured

---
+++ components/Home/Banner/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Banner = () => {
+	return (
+		<div>
+			<h3 className="text-3xl text-neutral-50 p-64 text-center bg-green-500">
+				This is banner
+			</h3>
+		</div>
+	);
+};
+
+export default Banner;
---
+++ components/Home/Features/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Features = () => {
+	return (
+		<div>
+			<p className="text-center text-red-600 bg-blue-300 p-32">
+				This is features section
+			</p>
+		</div>
+	);
+};
+
+export default Features;
