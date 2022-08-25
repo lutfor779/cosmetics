df8e9ee6fd9a1cc5aaab547e25a109e3dd6cc4b7
lutfor779 <mdlrrahman779@gmail.com>
2022-08-25 10:54:05 +0600

    navbar done

---
+++ client/components/Shared/Header/Tools/SingleItem.js
@@ -0,0 +1,37 @@
+import React from "react";
+import styles from "../../../../styles/Header.module.css";
+
+const SingleItem = ({ IconName, number, hoverData }) => {
+	return (
+		<li
+			className={`${styles.showOption} inline-block relative [&:nth-child(2)]:mx-3`}
+		>
+			<IconName className="w-12 h-12 p-3 rounded-full bg-white shadow hover:bg-[#f85457] hover:text-white transition-all" />
+
+			{/* DISPLAY ON HOVER */}
+			{hoverData && (
+				<div
+					className={`${styles.options} w-64 bg-white absolute right-0 z-20 `}
+				>
+					<ul className="p-5">
+						{hoverData.map((item) => (
+							<li key={item} className="pb-1 mb-2 border-b">
+								{item}
+							</li>
+						))}
+						<li>LOGIN</li>
+					</ul>
+				</div>
+			)}
+
+			{/* BADGE */}
+			{number && (
+				<div className="inline-flex absolute -top-2 -right-2 justify-center items-center w-5 h-5 text-xs font-bold text-white bg-[#f85457] rounded-full border border-white">
+					{number}
+				</div>
+			)}
+		</li>
+	);
+};
+
+export default SingleItem;
--- client/components/Shared/Header/Tools/index.js
+++ client/components/Shared/Header/Tools/index.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { CgHeart, CgProfile, CgShoppingCart } from "react-icons/cg";
+import SingleItem from "./SingleItem";
+
+const data = ["MY ACCOUNT", "WISHLIST", "CHECKOUT", "BLOG"];
 
 const index = () => {
 	return (
-		<div>
-			<p>Tools</p>
-		</div>
+		<ul>
+			<SingleItem IconName={CgHeart} number={25} />
+			<SingleItem IconName={CgShoppingCart} number={5} />
+			<SingleItem IconName={CgProfile} hoverData={data} />
+		</ul>
 	);
 };
 
