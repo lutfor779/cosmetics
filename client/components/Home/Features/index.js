f4a99506d317239ef566b52235c89f253c753f3e
keya12345 <0319452930@student.wub.edu.bd>
2022-08-25 22:47:59 +0600

    check branch

--- client/components/Home/Features/index.js
+++ client/components/Home/Features/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 const Features = () => {
-	return (
-		<div>
-			<p className="text-center text-red-600 bg-blue-300 p-32">
-				This is features section
-			</p>
-		</div>
-	);
+  return (
+    <div>
+      <p className="text-center text-red-600 bg-blue-300 p-32">
+        This is treatment section
+      </p>
+    </div>
+  );
 };
 
 export default Features;
