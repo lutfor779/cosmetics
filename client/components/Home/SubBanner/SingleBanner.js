d3c82711925a0bb966a8fd36d1df403459d79873
lutfor779 <mdlrrahman779@gmail.com>
2022-08-25 21:34:34 +0600

    bug fixed on sub banner

--- client/components/Home/SubBanner/SingleBanner.js
+++ client/components/Home/SubBanner/SingleBanner.js
@@ -11,11 +11,13 @@ const SingleBanner = ({ details }) => {
 
 			{/* content */}
 			<div
-				className={`absolute top-0 w-full h-full flex justify-${position} items-${
-					position === "center" ? "start" : "center"
-				} p-8 md:p-16`}
+				className={`absolute top-0 w-full h-full flex ${
+					position === "end" ? "justify-end" : `justify-${position}`
+				} items-${position === "center" ? "start" : "center"} p-8 md:p-16`}
 			>
-				<div className={`text-${position}`}>
+				<div
+					className={`${position === "end" ? "text-end" : `text-${position}`}`}
+				>
 					<h3 className="text-3xl">
 						{words[0] + " " + words[1]} <br />
 						{words.slice(2)?.map((item) => item + " ")}
