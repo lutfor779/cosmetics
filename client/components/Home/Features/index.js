608fab17d90b66633986ae383fcc100dc017584e
keya12345 <0319452930@student.wub.edu.bd>
2022-08-27 19:24:51 +0600

    set special treatments

--- client/components/Home/Features/index.js
+++ client/components/Home/Features/index.js
@@ -1,13 +1,52 @@
 import React from "react";
+import img1 from "../../../assets/images/homepage/feature/icon1.png";
+import img2 from "../../../assets/images/homepage/feature/icon2.png";
+import img3 from "../../../assets/images/homepage/feature/icon3.png";
+import img4 from "../../../assets/images/homepage/feature/icon4.png";
+import SingleFeatures from "./SingleFeatures";
 
-const Features = () => {
+const data = [
+  {
+    id: 1,
+    image: img1,
+    content1: "Hardware",
+    content2: "cosmetology",
+  },
+  {
+    id: 2,
+    image: img2,
+    content1: "Aesthetic",
+    content2: "cosmetology",
+  },
+  {
+    id: 3,
+    image: img3,
+    content1: "Injection",
+    content2: "cosmetology ",
+  },
+  {
+    id: 4,
+    image: img4,
+    content1: "Laser",
+    content2: "cosmetology",
+  },
+];
+
+const Feature = () => {
   return (
-    <div>
-      <p className="text-center text-red-600 bg-blue-300 p-32">
-        This is treatment section
+    <div className="m-8">
+      <h2 className="text-center font-medium text-4xl">Special Treatments</h2>
+      <p className="text-center m-2">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry
       </p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+        {data.map((item) => (
+          <SingleFeatures key={item.id} details={item} />
+        ))}
+      </div>
     </div>
   );
 };
 
-export default Features;
+export default Feature;
