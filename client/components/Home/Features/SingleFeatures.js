a0d73879943b68de1adbf662c9a90cc1330777ff
keya12345 <0319452930@student.wub.edu.bd>
2022-08-27 19:32:27 +0600

    tretments

--- client/components/Home/Features/SingleFeatures.js
+++ client/components/Home/Features/SingleFeatures.js
@@ -3,13 +3,14 @@ import React from "react";
 
 const SingleBanner = ({ details }) => {
   const { image, content1, content2 } = details;
-  //const words = content1, content2;
 
   return (
-    <div className="justify-end">
+    <div className="justify-between text-center">
       <Image src={image} alt="image" />
-      <h4>{content1}</h4>
-      <h4>{content2}</h4>
+      <div className="text-center">
+        <h4>{content1}</h4>
+        <h4>{content2}</h4>
+      </div>
     </div>
   );
 };
