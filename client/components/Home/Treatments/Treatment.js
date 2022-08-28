e09d6803c8820ad5a72bdca5f9feb40d5a113420
lutfor779 <mdlrrahman779@gmail.com>
2022-08-29 00:02:37 +0600

    treatments done

--- client/components/Home/Treatments/Treatment.js
+++ client/components/Home/Treatments/Treatment.js
@@ -1,15 +1,58 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
+import PlusIcon from "../../Shared/PlusIcon";
 
-const Treatment = ({ details }) => {
-	const { image, content1, content2 } = details;
+const Treatment = ({ content }) => {
+	const { image, content1, content2, bgImage, details } = content;
+
+	const [hover, setHover] = useState(false);
+
+	const hoverStyle = {
+		backgroundImage: `url(${bgImage.src})`,
+		backgroundRepeat: "no-repeat",
+		backgroundSize: "cover",
+		backgroundPosition: "center",
+	};
 
 	return (
-		<div className="justify-between text-center m-4">
-			<Image src={image} alt="image" />
-			<div className="text-center">
-				<h4>{content1}</h4>
-				<h4>{content2}</h4>
+		<div
+			className={`justify-star border relative`}
+			onMouseEnter={() => {
+				setHover(true);
+			}}
+			onMouseLeave={() => {
+				setHover(false);
+			}}
+		>
+			<div className="h-full" style={hover ? hoverStyle : null}>
+				<div className={`p-12  ${hover ? "hidden" : "block"}`}>
+					<div className="bg-white">
+						<Image src={image} alt="image" />
+
+						<div className="text-xl font-medium mt-3">
+							<h4>{content1}</h4>
+							<h4>{content2}</h4>
+						</div>
+					</div>
+				</div>
+
+				{/* onHover */}
+				<div
+					className={`h-full bg-[#ffffffbb] p-12 ${hover ? "block" : "hidden"}`}
+				>
+					<div className="h-full flex flex-col justify-between relative">
+						<div className="text-xl font-medium">
+							<h4>{content1}</h4>
+							<h4>{content2}</h4>
+						</div>
+						<p>{details}</p>
+
+						{/* icon */}
+						<div className="absolute -top-7 right-0">
+							<PlusIcon />
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
--- client/components/Home/Treatments/index.js
+++ client/components/Home/Treatments/index.js
@@ -1,8 +1,12 @@
 import React from "react";
-import img1 from "../../../assets/images/homepage/feature/icon1.png";
-import img2 from "../../../assets/images/homepage/feature/icon2.png";
-import img3 from "../../../assets/images/homepage/feature/icon3.png";
-import img4 from "../../../assets/images/homepage/feature/icon4.png";
+import bg4 from "../../../assets/images/background/treatments//treatment4.jpg";
+import bg1 from "../../../assets/images/background/treatments/treatment1.png";
+import bg2 from "../../../assets/images/background/treatments/treatment2.jpg";
+import bg3 from "../../../assets/images/background/treatments/treatment3.jpg";
+import img1 from "../../../assets/images/homepage/treatments/icon1.png";
+import img2 from "../../../assets/images/homepage/treatments/icon2.png";
+import img3 from "../../../assets/images/homepage/treatments/icon3.png";
+import img4 from "../../../assets/images/homepage/treatments/icon4.png";
 import Treatment from "./Treatment";
 
 const data = [
@@ -11,41 +15,54 @@ const data = [
 		image: img1,
 		content1: "Hardware",
 		content2: "cosmetology",
+		bgImage: bg1,
+		details: "Reveal your natural Cosmitek and show the path to excellence",
 	},
 	{
 		id: 2,
 		image: img2,
 		content1: "Aesthetic",
 		content2: "cosmetology",
+		bgImage: bg2,
+		details: "Reveal your natural Cosmitek and show the path to excellence",
 	},
 	{
 		id: 3,
 		image: img3,
 		content1: "Injection",
 		content2: "cosmetology ",
+		bgImage: bg3,
+		details: "Reveal your natural Cosmitek and show the path to excellence",
 	},
 	{
 		id: 4,
 		image: img4,
 		content1: "Laser",
 		content2: "cosmetology",
+		bgImage: bg4,
+		details: "Reveal your natural Cosmitek and show the path to excellence",
 	},
 ];
 
 const Treatments = () => {
 	return (
-		<div className="m-8">
-			<h2 className="text-center font-medium text-4xl">Special Treatments</h2>
-			<p className="text-center m-2">
-				Lorem Ipsum is simply dummy text of the printing and typesetting
-				industry
-			</p>
-			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+		<section className="container m-auto p-5 py-12">
+			{/* title */}
+			<div>
+				<h2 className="text-center font-medium text-4xl">Special Treatments</h2>
+				<p className="text-center m-2">
+					Lorem Ipsum is simply dummy text of the printing and typesetting
+					industry
+				</p>
+			</div>
+
+			{/* content */}
+			<div className="py-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
 				{data.map((item) => (
-					<Treatment key={item.id} details={item} />
+					<Treatment key={item.id} content={item} />
 				))}
 			</div>
-		</div>
+		</section>
 	);
 };
 
