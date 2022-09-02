ca42e400a21ea23d90465498788627520356c7e8
keya12345 <0319452930@student.wub.edu.bd>
2022-09-02 10:17:17 +0600

    add drag and drop

--- client/pages/admin/products/add.js
+++ client/pages/admin/products/add.js
@@ -4,10 +4,41 @@ const Add = () => {
   return (
     <div className="container m-auto ">
       <h1 className="text-2xl m-4 font-medium">Add Product</h1>
-      <div className="grid grid-cols-3 gap-4 m-2 mb-8">
-        <div className="bg-orange-400 mt-20 ">
-          <h1 className="text-5xl font-bold text-center">765 * 850</h1>
-          <p>Please choose image according to the aspect ratio</p>
+      <div className="grid  lg:grid-cols-3 gap-4 m-2 mb-8 sm:grid-cols-1">
+        <div className=" mt-20 ">
+          <div class="flex justify-center items-center w-full">
+            <label
+              for="dropzone-file"
+              class="flex flex-col justify-center items-center w-full h-64 bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+            >
+              <div class="flex flex-col justify-center items-center pt-5 pb-6">
+                <svg
+                  aria-hidden="true"
+                  class="mb-3 w-10 h-10 text-gray-400"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                  ></path>
+                </svg>
+                <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                  <span class="font-semibold">Click to upload</span> or drag and
+                  drop
+                </p>
+                <p class="text-xs text-gray-500 dark:text-gray-400">
+                  SVG, PNG, JPG or GIF (MAX. 800x400px)
+                </p>
+              </div>
+              <input id="dropzone-file" type="file" class="hidden" />
+            </label>
+          </div>
+
           <div className="grid grid-cols-3 gap-3 mt-24 text-center">
             <div>
               <p className="text-center">
@@ -108,7 +139,7 @@ const Add = () => {
         </div>
 
         {/* 2nd part */}
-        <div className="col-span-2 bg-teal-400 mx-3 m-2">
+        <div className="col-span-2  mx-3 m-2">
           <div className="grid grid-cols-2">
             <div className=" text-sm font-medium text-slate-700">
               <h1>PRODUCT NAME</h1>
